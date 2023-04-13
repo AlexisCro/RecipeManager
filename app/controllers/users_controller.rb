88b1a40087dec41fb653ff7b9060b8f004e777d1
AlexisCro <95683483+AlexisCro@users.noreply.github.com>
2023-04-13 13:59:34 +0200

    manage modify profile

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,22 +1,36 @@
 class UsersController < ApplicationController
 
+    skip_before_action :only_signed_in, only: %i[new create confirm]
+
     def new
         @user = User.new
     end
 
     def create
-        user_params = params.require(:user).permit %i[username mail firstname lastname password ]
+        user_params = params.require(:user).permit %i[username mail firstname lastname password password_confirmation]
         @user = User.new(user_params)
         if @user.valid?
             @user.save
             UserMailer.confirm(@user).deliver_now
-            redirect_to action: :new, success: t('register.mailer.confirm_message')
+            redirect_to action: :new, success: t('mailer.confirm_message')
         else
-            puts @user.valid?
-            if @user.errors.any?
-                @user.errors.full_messages.each { |error| puts error}
-            end
-            render 'new', danger: 'problem'
+            @user.errors.full_messages.each { |error| puts error }
+            render action: :new, danger: @user.errors.full_messages
+        end
+    end
+
+    def edit
+        @user = User.find(session[:auth]['id'])
+    end
+
+    def update
+        @user = User.find(session[:auth]['id'])
+        if @user.update(update_params)
+            flash[:success] = t('edit.success')
+            redirect_to profil_path
+        else
+            flash[:danger] = t('edit.danger')
+            redirect_to profil_path
         end
     end
 
@@ -25,8 +39,16 @@ class UsersController < ApplicationController
         if @user.confirmation_token == params[:token]
             @user.update(is_confirmed: true, confirmation_token: nil)
             @user.save(validates: false)
+            session[:auth] = {id: @user.id}
+            redirect_to profil_path
         else
             redirect_to new_user_path, danger: t('register.mailer.invalid_token')
         end
+    end
+
+    private
+
+    def update_params
+        params.require(:user)[:password].blank? ? params.require(:user).permit %i[mail firstname lastname] : params.require(:user).permit(User::UPDATE_PARAMS)
     end
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -2,12 +2,22 @@ class User < ApplicationRecord
 
     has_secure_password
     has_secure_token :confirmation_token
+
+    validates :firstname, presence: true
+    validates :lastname, presence: true
+
     validates :username, 
         format: { with: /[a-zA-Z0-9]/, message: 'invalid username' },
         uniqueness: { case_sensitive: false }
 
     validates :mail, 
-        format: { with: /\A[a-zA-Z0-9._-]+@[a-zA-Z0-9._-]+.[a-zA-Z.]/, message: 'invalid mail'}, 
-        uniqueness: { case_sensitive: false }
+        format: { with: /\A[a-zA-Z0-9._-]+@[a-zA-Z0-9._-]+.[a-zA-Z.]/, message: 'invalid mail'},
+        uniqueness: { case_sensitive: false },
+        on: %i[create]
 
+    UPDATE_PARAMS = %i[mail lastname firstname password password_confirmation]
+
+    def to_session
+        {id: id}
+    end
 end
