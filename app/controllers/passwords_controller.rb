d8280e0554913c09c31f5d0f15a925870f966fee
AlexisCro <95683483+AlexisCro@users.noreply.github.com>
2023-04-25 08:29:18 +0200

    start recover password

---
+++ app/controllers/passwords_controller.rb
@@ -0,0 +1,19 @@
+class PasswordsController < ApplicationController
+
+    skip_before_action :only_signed_in
+    before_action :only_signed_out
+
+    def new; end
+
+    def create  
+        user_params = params.require(:user)
+        @user = User.find_by_email(user_params[:email])
+        if @user
+          @user.regenerate_recover_password_token #method according by has_secure_token
+          UserMailer.password(@user).deliver_now
+          redirect_to new_session_path, success: an email sent you
+        else
+          redirect_to new_password_path, danger: 'no user correspond'
+        end
+    end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -2,16 +2,19 @@ Rails.application.routes.draw do
 
   root "users#new"
 
-  get '/profil', to: 'users#edit', as: :profil
-  post '/profil', to: 'users#update'
-
+  # Users routes 
   resources :users, only: %i[new create edit] do 
     member do 
       get 'confirm'
     end
   end
 
-  #Session CRUD
+  get '/profil', to: 'users#edit', as: :profil
+  post '/profil', to: 'users#update'
+
+  resources :password, only: %i[new create edit update]
+
+  # Session CRUD
   get '/login', to: 'sessions#new', as: :new_session
   post '/login', to: 'sessions#create'
   delete '/logout', to: 'sessions#destroy', as: :destroy_session
