180788acfa3763adcea9330cdb19e362a62fce60
AlexisCro <95683483+AlexisCro@users.noreply.github.com>
2023-04-27 14:15:43 +0200

    finish recover password

--- app/controllers/passwords_controller.rb
+++ app/controllers/passwords_controller.rb
@@ -7,8 +7,8 @@ class PasswordsController < ApplicationController
     end
 
     def create  
-        user_params = params.require(:user)
-        @user = User.find_by_email(user_params[:email])
+        user_params
+        @user = User.find_by(mail: user_params[:email])
         if @user
           @user.regenerate_recover_password_token #method according by has_secure_token
           UserMailer.password(@user).deliver_now
@@ -19,8 +19,30 @@ class PasswordsController < ApplicationController
     end
 
     def edit
+      @user = User.find(params[:id])
+      if @user.recover_password_token != params[:token].delete_prefix("3D")
+        redirect_to new_password_path, danger: 'wrong token'
+      end
     end
 
     def update
+      @user = User.find(params[:id])
+      if @user.recover_password_token === user_params[:recover_password_token]
+        @user.assign_attributes(user_params.permit(:password, :password_confirmation))
+        if @user.valid?
+          @user.recover_password_token = nil
+          @user.save
+          session[:auth] = @user.to_session
+          redirect_to profil_path, success: 'Your password has been reinitialized'
+        end
+      else
+        redirect_to new_password_path, danger: 'wrong token'
+      end
+    end
+
+    private
+
+    def user_params
+      params.require(:user)
     end
 end
