df6c9944d9ce4663187eb64cbb136f538b897fc6
AlexisCro <95683483+AlexisCro@users.noreply.github.com>
2023-07-18 13:29:37 +0200

    wrap only in devlopment

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,7 +1,9 @@
 module ApplicationHelper
   def render_which_partial(partial_name, &block)
-    "<!-- Start to the partial #{partial_name} -->".html_safe +
-      capture(&block) +
-      "<!-- End to the partial #{partial_name} -->".html_safe
+    if Rails.env.development?
+      "<!-- Start to the partial #{partial_name} -->".html_safe +
+        capture(&block) +
+        "<!-- End to the partial #{partial_name} -->".html_safe
+    end
   end
 end
