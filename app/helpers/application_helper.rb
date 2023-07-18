43fb724c27c6f48b525e11345be4e259d118bf0b
AlexisCro <95683483+AlexisCro@users.noreply.github.com>
2023-07-18 13:26:02 +0200

    wrap partial

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,7 @@
 module ApplicationHelper
+  def render_which_partial(partial_name, &block)
+    "<!-- Start to the partial #{partial_name} -->".html_safe +
+      capture(&block) +
+      "<!-- End to the partial #{partial_name} -->".html_safe
+  end
 end
